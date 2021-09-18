da60cb2fdd0ee1397fd93d23b062685e05be48a8
julianpizarroclear <jpizarro@unimagdalena.edu.co>
2021-09-18 14:45:59 -0500

    metodos de consulta a la bd

---
+++ backend/src/app/api/services.py
@@ -0,0 +1,29 @@
+from sqlalchemy.orm import Session
+from .schemas import MedicionView, Medicion, MedicionQf
+from . import models, schemas
+
+
+def conveter_medicion(medicion):
+    return MedicionView(id = medicion.id, estacion = medicion.estacion.nombre,
+        parametro= medicion.parametro.nombre,
+        fecha=medicion.fecha,
+        valor=medicion.valor,
+        unidad= medicion.parametro.unidad,
+        qf = medicion.qf
+    )
+def get_mediciones(db:Session, skip: int = 0, limit: int=20):
+    ms = db.query(models.Medicion).offset(skip).limit(limit).all()
+
+    mediciones = [conveter_medicion(medicion) for medicion in ms]
+
+    return mediciones
+
+def get_medicion(db: Session, medicion_id: int):
+    return db.query(models.Medicion).filter(models.Medicion.id == medicion_id).first()
+
+def update_qf_medicion(db:Session, medicion: MedicionQf):
+    md = db.query(models.Medicion).filter(models.Medicion.id == medicion.id).first()
+    md.qf = medicion.qf
+    db.commit()
+    db.refresh(md)
+    return conveter_medicion(md)
