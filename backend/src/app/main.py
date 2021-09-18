511441444f6ed3e766b749ef82e2b4b30def67d4
julianpizarroclear <jpizarro@unimagdalena.edu.co>
2021-09-18 14:47:06 -0500

    agregado CORS

--- backend/src/app/main.py
+++ backend/src/app/main.py
@@ -1,13 +1,30 @@
 from fastapi import FastAPI
-
-#from app.database import engine, database, metadata 
-#metadata.create_all(engine)
+from fastapi.middleware.cors import CORSMiddleware
 from app.db import engine
 from app.api import models
+from app.api.monitoreos import router
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+    "*",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.router.prefix = "/api/v1"
+
+app.include_router(router)
+
 '''
 @app.on_event("startup")
 async def startup():
