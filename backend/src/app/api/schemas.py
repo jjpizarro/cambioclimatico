ca50d31e0255361efc6927731128acf227f91457
julianpizarroclear <jpizarro@unimagdalena.edu.co>
2021-09-19 15:54:29 -0500

    esquemas para el manejo de usuario

--- backend/src/app/api/schemas.py
+++ backend/src/app/api/schemas.py
@@ -1,6 +1,6 @@
 from typing import List, Optional
 from .models import QualityFlag
-from pydantic import BaseModel
+from pydantic import BaseModel,EmailStr
 from datetime import datetime
 
 
@@ -46,4 +46,37 @@ class Filtro(BaseModel):
     parametro:int
     startdate: datetime
     enddate: datetime
-    
+
+
+class UserBase(BaseModel):
+    name: Optional[str]
+    email: Optional[EmailStr] = None
+    is_superuser: bool = False
+
+
+# Properties to receive via API on creation
+class UserCreate(UserBase):
+    email: EmailStr
+    password: str
+
+
+# Properties to receive via API on update
+class UserUpdate(UserBase):
+    ...
+
+
+class UserInDBBase(UserBase):
+    id: Optional[int] = None
+
+    class Config:
+        orm_mode = True
+
+
+# Additional properties stored in DB but not returned by API
+class UserInDB(UserInDBBase):
+    hashed_password: str
+
+
+# Additional properties to return via API
+class User(UserInDBBase):
+    ...
