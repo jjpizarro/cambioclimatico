cafd7f00bf94f33529afc82f1d45c324a7fdc519
julianpizarroclear <jpizarro@unimagdalena.edu.co>
2021-09-26 09:29:21 -0500

    servicio para interactuar con la api de autenticació

---
+++ frontend/src/services/auth.service.js
@@ -0,0 +1,35 @@
+import axios from "axios";
+const http = axios.create({
+    baseURL: "http://localhost/api/v1/"
+  });
+
+const register = (username, email, password)=>{
+    return http.post('/signup',{username, email, password});
+}
+
+const login = (username, password)=>{
+    return http.post('/login',JSON.stringify(`grant_type=&username=${username}&password=${password}&scope=&client_id=&client_secret=`),{
+        
+          "Content-type": "application/x-www-form-urlencoded"
+        }).
+    then((response)=>{
+        if(response.data.access_token){
+            localStorage.setItem('user',JSON.stringify(response.data));
+        }
+        return response.data;
+    });
+}
+const logout =()=>{
+    localStorage.removeItem('user');
+}
+
+const getCurrentUser = ()=>{
+    return JSON.parse(localStorage.getItem('user') );
+}
+
+export default{
+    register,
+    login,
+    logout,
+    getCurrentUser
+}
