08991ae532d9a223c653bd45fff2405f8174d402
julianpizarroclear <jpizarro@unimagdalena.edu.co>
2021-09-26 09:31:32 -0500

    se ajustan las rutas y se agregan las publicas y privadas

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -1,10 +1,21 @@
-import React from 'react'
-import {Route, Switch} from "react-router-dom"
+import React, {useContext} from 'react'
+import {Switch} from "react-router-dom"
 import Mediciones from './pages/Mediciones'
+import Login from './pages/Login';
+import PublicRoute from './components/PublicRoute';
+import PrivateRoute from './components/PrivateRoute';
+import { AuthContext }  from './context/Auth'
+
 export default function Routes(){
+    const { isAuthenticated } = useContext(AuthContext)
     return (
         <Switch>
-            <Route exact path="/" component={Mediciones} />
-        </Switch>
+             <PublicRoute exact path="/login" component={Login} />
+             <PrivateRoute exact path="/" 
+             isAuthenticated={isAuthenticated}
+             redirect="/login"
+             component={Mediciones} />
+       </Switch>     
+       
     )
 }
