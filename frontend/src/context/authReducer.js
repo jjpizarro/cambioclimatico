5f7629e96d90276ed724c8f415d95c00a84e48bb
julianpizarroclear <jpizarro@unimagdalena.edu.co>
2021-09-26 09:27:48 -0500

    reducer para manejar el estado de autenticado

---
+++ frontend/src/context/authReducer.js
@@ -0,0 +1,25 @@
+/*
+initialState = {
+    isAuthenticated:false,
+    user:null
+}
+*/
+
+export const AuthReducer = (state, action)=>{
+    switch(action.type){
+        case 'LOGIN':
+            return {
+                ...state,
+                isAuthenticated:true,
+                user:action.payload
+            };
+        case 'LOGOUT':
+            localStorage.clear();
+            return {
+                ...state,
+                isAuthenticated:false,
+                user:null
+            }
+
+    }
+}
