b2a8f198a58829c715339e7a44decf0035038887
julianpizarroclear <jpizarro@unimagdalena.edu.co>
2021-09-26 09:25:49 -0500

    componente para hacer privada y pública las rutas

---
+++ frontend/src/components/PublicRoute.js
@@ -0,0 +1,36 @@
+//https://github.com/ronaksonigara/react-private-public-route/blob/main/src/components/PublicRoute.js
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
+import PropTypes from "prop-types";
+
+const propTypes = {
+	component: PropTypes.func.isRequired,
+	redirect: PropTypes.string,
+	restricted: PropTypes.bool,
+};
+
+const defaultProps = {
+	restricted: false,
+	redirect: "/",
+};
+
+const PublicRoute = ({
+	component: Component,
+	redirect: pathname,
+	restricted,
+	...rest
+}) => {
+	return (
+		<Route
+			{...rest}
+			render={(props) =>
+				restricted ? <Redirect to={{ pathname }} /> : <Component {...props} />
+			}
+		/>
+	);
+};
+
+PublicRoute.propTypes = propTypes;
+PublicRoute.defaultProps = defaultProps;
+
+export default PublicRoute;
